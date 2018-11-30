84fd3c3f08a76c65bbe4b6b40b9b81cc81697c57
Sergey Shliakhov <ss@applemint.eu>
2018-11-30 14:29:26 +0200

    edit lambda deploy command

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ git clone https://github.com/matchish/placeinqueue.git frontend
 git clone https://github.com/matchish/placeinqueue-api.git api
 git clone https://github.com/matchish/placeinqueue-api.git lambda
 
-docker-compose exec lambda deploy
+docker-compose exec serverless deploy
 docker-compose exec api npm install && npm migrate
 docker-compose up -d mariadb api frontend
