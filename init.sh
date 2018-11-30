dd14af94bbfc25a6af82a45246f9aecd7da9f3ba
Sergey Shliakhov <ss@applemint.eu>
2018-11-30 14:26:44 +0200

    init

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/matchish/placeinqueue.git frontend
+git clone https://github.com/matchish/placeinqueue-api.git api
+git clone https://github.com/matchish/placeinqueue-api.git lambda
+
+docker-compose exec lambda deploy
+docker-compose exec api npm install && npm migrate
+docker-compose up -d mariadb api frontend
