77e3a80634483358c5366f9be4b9e63e56fea29a
Sergey Shliakhov <ss@applemint.eu>
2018-12-05 09:21:35 +0200

    Unify env section

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alp
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-api.git browser
 
 docker-compose -f docker-compose-prod.yml up -d db api
-docker-compose exec api npm migrate
+docker-compose exec api npm run migrate
