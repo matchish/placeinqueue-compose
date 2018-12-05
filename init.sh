ac2af74d8eedf6adb14f5369179a3ac1015ca360
Sergey Shliakhov <ss@applemint.eu>
2018-12-05 14:48:52 +0200

    Hotfix

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@ docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alp
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-api.git api
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-api.git browser
 
+# TODO: remove hack
+mkdir frontend/dist
+chown 1000:1000 frontend/dist
+
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml up -d db api frontend nginx
 docker-compose exec api npm run migrate
