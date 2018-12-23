64378e35c36a0896f203a27708ae4c22571cac94
Sergey Shliakhov <ss@applemint.eu>
2018-12-23 08:37:16 +0200

    Add dispatcher service

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue.git frontend
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-api.git api
 docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-lambda.git browser
+docker run -ti --rm -v ${HOME}:/root -v $(pwd):/git --user $(id -u):$(id -g) alpine/git clone https://github.com/matchish/placeinqueue-dispatcher.git dispatcher
 
 # TODO: remove hack
 mkdir frontend/dist
@@ -10,4 +11,5 @@ sudo chown 1000:1000 frontend/dist
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml up -d api nginx
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml run --rm frontend npm run build
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml run --rm browser deploy
+docker-compose -f docker-compose.yml -f prod.docker-compose.yml run --rm dispatcher deploy
 docker-compose exec api npm run migrate
